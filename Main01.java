b17aa6db21891db5ec9d9cf8c5a20f14a5c1e872
Yorki06 <dawid.inczewski@onet.pl>
2021-11-14 19:15:27 +0100

    Initial commit

---
+++ Main01.java
@@ -0,0 +1,21 @@
+public class Main01 {
+
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x);
+
+        char NewChar = 'A';
+        System.out.println(NewChar);
+
+        long Longvar = 5123141;
+        System.out.println(Longvar);
+
+        double newDouble = 46;
+        System.out.println(newDouble);
+
+        boolean newBool = (4 ==5);
+        System.out.println(newBool);
+
+
+    }
+}
---
+++ Main04.java
@@ -0,0 +1,11 @@
+public class Main04 {
+
+    public static void main(String[] args) {
+
+        String str1  = "Kurs";
+        String str2  = "Java";
+        String joinedStrings = (str1 + " " + str2);
+        System.out.println(joinedStrings); // Kurs Java
+
+    }
+}
